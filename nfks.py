4e41f10360cb0295e1b2b28bf07df562fab5afe0
Michael Chapman <michael.chapman@anu.edu.au>
2012-11-26 09:44:23 +1100

    Can add users to projects, add projects, wirte cred files

--- nfks.py
+++ nfks.py
@@ -2,8 +2,10 @@
 from optparse import OptionParser
 from nfacct import DataBase, researchers
 
-from sqlalchemy import Table, MetaData, Column, ForeignKey, Integer, String, create_engine
-from sqlalchemy.orm import mapper, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
 from urllib import quote_plus as urlquote
 
 import os, sha, base64, random, string
@@ -21,13 +23,22 @@ db = create_engine(os.environ.get('NF_DRIVER') + '://' +
                    os.environ.get('NF_HOST') + '/' +
                    os.environ.get('NF_DATABASE'))
 
+Base = declarative_base()
+Base.metadata.reflect(bind=db)
+
+class Projects(Base):
+    __table__ = Base.metadata.tables['projects']
+
+class Researchers(Base):
+    __table__ = Base.metadata.tables['researchers']
+
+class People(Base):
+    __table__ = Base.metadata.tables['people']
+
 Session = sessionmaker(bind=db)
 session = Session()
-metadata = MetaData(db)
 
-projects = Table('projects', metadata, autoload=True)
-users = Table('researchers', metadata, autoload=True)
-
+memberRole='fd285496b39e42a8a9949881c36a9603'
 
 # Generate a pseudo-random salt
 def getsalt(chars = string.letters + string.digits,length=16):
@@ -54,34 +65,56 @@ def add_project(project):
         print "That project has no users."
         return
 
+    title =  session.query(Projects, Projects.title).filter_by(project=project)[0].title
+
     # get tenant by name is bugged at the moment
     # https://bugs.launchpad.net/keystone/+bug/1055763
     tenants = keystone.tenants.list()
     t = [t for t in tenants if t.name==project]
-    if len(t) > 0:
-        tenant = t[0]
-    else:
-        print type()
-        pass
-        #keystone.tenants.create(project, description="", enabled="")
+    if len(t) == 0:
+        print "making project " + project + " with description '" + title + "'"
+        keystone.tenants.create(project, description=title, enabled=True)
 
-    # if the project already exists
-    
-# add user in reseracher db to tenant in keystone
+    tenant = [t for t in tenants if t.name==project][0]
+    existingusers = [k.name for k in keystone.users.list()]
+
+    for user in users:
+        if user in existingusers:
+            print "adding member roles to " + user
+            keystone.roles.add_user_role(user.id, role=memberRole, tenant=tenant.id)
+        else:
+            print "adding " + user
+            userpw = str(base64.encodestring(sha.new(randpasswd()).digest())).replace('=','-')[:-1]
+            pid = researchers.get_personid(db, user)
+
+            # some users don't have email addresses
+            ppl = session.query(People.email).filter_by(personid=pid)
+            for p in ppl:
+                email = p.email
+                break
+            keystone.users.create(name=user, password=userpw, email=email, tenant_id=tenant.id, enabled=True)
+            create_cred_file(user, userpw, tenant.name)
+            
+# add user in researcher db to tenant in keystone
 def add_user_to_tenant(user, tenant):
     keystone = client.Client(username=username, password=password, tenant_name=tenant_name, auth_url=auth_url)
     db = DataBase.acctDB()
     
-# write user credentials to a file for them to source
-def write_credentials(user, tenant, password):
-    pass
-    
 def test():
     keystone = client.Client(username=username, password=password, tenant_name=tenant_name, auth_url=auth_url)
-    tenants = keystone.tenants.list()
+    print keystone.tenants.list()[0].name
+    title =  session.query(Projects, Projects.title).filter_by(project='z00')
+    print title[0].title
 
-    for project in session.query(projects):
-        print project.title
+# write user credentials to a file for them to source
+def create_cred_file(user, password, tenant, keystone, tenant):
+    with open(tenant + '/' + user + 'rc', 'w') as credfile:
+        credfile.write('export OS_USERNAME=' + user + '\n')
+        credfile.write('export OS_TENANT_NAME=' + tenant + '\n')
+        credfile.write('export OS_AUTH_URL=' + auth_url + '\n')
+        credfile.write('export OS_REGION_NAME=' + 'RegionOne' + '\n')
+        credfile.write('export OS_PASSWORD=' + users[user][1] + '\n')
+        credfile.write('export OS_NO_CACHE=1'+ '\n')
 
 
 def main():
@@ -95,18 +128,17 @@ def main():
     p.add_option('--debug', '-d')
     options, arguments = p.parse_args()
 
-    # add project, and all users from project
-    # output creds for new users
     if (options.debug):
         test()
+        return
 
     if (options.project):
         add_project(options.project)
+        return
 
-    # add user who is in project to tenant
-    # no creds output
     if (options.user and options.tenant):
         add_user_to_tenant(options.user, options.tenant)
+        return
 
 if __name__ == '__main__':
     main()
